98ecb9a9bb9ab59a2f1aef779728e1efbb876083
Bertrand Lemasle <blemasle@gmail.com>
2018-02-12 00:30:53 +0100

    Fixed PROGMEM string reading after sleep

--- GpsTracker/MainUnit.cpp
+++ GpsTracker/MainUnit.cpp
@@ -3,7 +3,7 @@
 #include "Pins.h"
 #include "Debug.h"
 
-const char WOKE_UP[] PROGMEM = "Woke up from sleep";
+const char WOKE_UP[] PROGMEM = "Woke up from sleep\n";
 
 namespace mainunit {
 
@@ -21,21 +21,19 @@ namespace mainunit {
 	}
 
 	void sleep(period_t period) {
-		Log.notice(F("Sleeping for period : %d"), period);
+		Log.notice(F("Sleeping for period : %d\n"), period);
 		
 		LowPower.powerDown(period, ADC_OFF, BOD_OFF);
 		
-		Log.verbose(reinterpret_cast<const __FlashStringHelper *>(pgm_read_word(WOKE_UP)));
+		Log.verbose(reinterpret_cast<const __FlashStringHelper *>(WOKE_UP));
 
 	}
 
 	void deepSleep(uint16_t seconds) {
-		Log.notice(F("Deep sleeping for %d seconds"), seconds);
+		Log.notice(F("Deep sleeping for %d seconds\n"), seconds);
 
 		interruptIn(seconds);
-		Log.verbose("interruptIn done !");
 		LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-		Log.verbose("hola");
-		Log.verbose(reinterpret_cast<const __FlashStringHelper *>(pgm_read_word(WOKE_UP)));
+		Log.verbose(reinterpret_cast<const __FlashStringHelper *>(WOKE_UP));
 	}
 }
