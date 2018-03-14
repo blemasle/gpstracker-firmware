30a99cb251bce63a46cdcdec89cf96e8fddac508
Bertrand Lemasle <blemasle@gmail.com>
2018-03-14 20:17:54 +1300

    Working network positions backup

--- GpsTracker/NetworkPositionsBackup.cpp
+++ GpsTracker/NetworkPositionsBackup.cpp
@@ -25,27 +25,24 @@ namespace positions {
 				bool appendPosition(PositionEntry &entry) {
 					VERBOSE("appendPosition");
 					debug::displayFreeRam();
-					delay(100);
+
 					char buffer[BUFFER_SIZE];
-					sprintf(buffer, "%d", 2100);
-					/*snprintf(buffer, BUFFER_SIZE, "%d,%d,%.2f,%d,",
+					snprintf(buffer, BUFFER_SIZE, "%d,%d,%d,%d,",
 						entry.metadata.batteryLevel,
 						entry.metadata.batteryVoltage,
-						entry.metadata.temperature,
-						static_cast<uint8_t>(entry.metadata.status));*/
+						static_cast<uint16_t>(entry.metadata.temperature * 100),
+						static_cast<uint8_t>(entry.metadata.status));
 
-					////strcat(buffer, entry.position);
-					//Serial.println(buffer);
+					strcat(buffer, entry.position);
 					debug::displayFreeRam();
-					return false;
 
-					/*return hardware::sim808::device.httpPost(
-						config.network.url,
-						PSTR("text/csv"),
+					return hardware::sim808::device.httpPost(
+						config::main::value.network.url,
+						F("text/csv"),
 						buffer,
 						buffer,
 						BUFFER_SIZE
-					) == POSITIONS_CONFIG_NET_DEFAULT_EXPECTED_RESPONSE;*/
+					) == POSITIONS_CONFIG_NET_DEFAULT_EXPECTED_RESPONSE;
 				}
 
 				void appendPositions() {
@@ -56,28 +53,28 @@ namespace positions {
 					PositionEntry currentEntry;
 					SIM808RegistrationStatus networkStatus;
 
-					/*network::powerOn();
+					network::powerOn();
 					networkStatus = network::waitForRegistered(NETWORK_DEFAULT_TOTAL_TIMEOUT_MS);
 
 					if (!network::isAvailable(networkStatus.stat)) VERBOSE_MSG("appendPositions", "network unavailable");
 					else if (!network::enableGprs()) VERBOSE_MSG("appendPositions", "gprs unavailable");
-					else {*/
+					else {
 						hardware::i2c::powerOn();
 						do {
 							if (!positions::get(currentEntryIndex, currentEntry)) break;
 							debug::displayFreeRam();
 
-							//if (!appendPosition(currentEntry)) break;
+							if (!appendPosition(currentEntry)) break;
 
-							/*config::main::value.network.lastSavedEntry = currentEntryIndex;
-							config::main::save();
-*/
+							config::main::value.network.lastSavedEntry = currentEntryIndex;
+							//config::main::save();
+
 						} while (positions::moveNext(currentEntryIndex));
 						debug::displayFreeRam();
 						hardware::i2c::powerOff();
-					//}
+					}
 
-					//network::powerOff();
+					network::powerOff();
 					debug::displayFreeRam();
 				}
 
