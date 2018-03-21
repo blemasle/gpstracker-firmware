f47ab930e4abd3db348c5aff156a2f221fe7a999
Bertrand Lemasle <blemasle@gmail.com>
2018-03-21 15:15:33 +1300

    Changed network backup configuration

--- GpsTracker/NetworkPositionsConfig.h
+++ GpsTracker/NetworkPositionsConfig.h
@@ -1,9 +1,9 @@
 #pragma once
 
 
-#define POSITIONS_CONFIG_NET_DEFAULT_SAVE_THRESHOLD		2
+#define POSITIONS_CONFIG_NET_DEFAULT_SAVE_THRESHOLD		10
 #define POSITIONS_CONFIG_NET_DEFAULT_APN				"Vodafone"
-#define POSITIONS_CONFIG_NET_DEFAULT_URL				"http://requestbin.fullcontact.com/16q71o61"
+#define POSITIONS_CONFIG_NET_DEFAULT_URL				"http://requestbin.fullcontact.com/1fgc99k1"
 #define POSITIONS_CONFIG_NET_DEFAULT_EXPECTED_RESPONSE	200
 
 
