2575be5e35220861c323ce51a008a355b1cd1c89
Bertrand Lemasle <blemasle@gmail.com>
2018-03-24 20:15:15 +1300

    Enabled network backup to real server

--- GpsTracker/NetworkPositionsConfig.h
+++ GpsTracker/NetworkPositionsConfig.h
@@ -3,8 +3,8 @@
 
 #define POSITIONS_CONFIG_NET_DEFAULT_SAVE_THRESHOLD		10
 #define POSITIONS_CONFIG_NET_DEFAULT_APN				"Vodafone"
-#define POSITIONS_CONFIG_NET_DEFAULT_URL				"http://requestbin.fullcontact.com/1fgc99k1"
-#define POSITIONS_CONFIG_NET_DEFAULT_EXPECTED_RESPONSE	200
+#define POSITIONS_CONFIG_NET_DEFAULT_URL				"http://yourserver.com/endpoint"
+#define POSITIONS_CONFIG_NET_DEFAULT_EXPECTED_RESPONSE	201
 
 
 struct networkConfig_t {
