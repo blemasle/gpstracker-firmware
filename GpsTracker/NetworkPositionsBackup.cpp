ce182f64f76aed09517aa4f89b5f5393740daf58
Bertrand Lemasle <blemasle@gmail.com>
2018-03-29 00:04:06 +1300

    Fixed intermediate timeout and data being incomplete / malformed

--- GpsTracker/NetworkPositionsBackup.cpp
+++ GpsTracker/NetworkPositionsBackup.cpp
@@ -23,20 +23,21 @@ namespace positions {
 
 			bool NetworkPositionsBackup::appendPosition(PositionEntry &entry) {
 				char buffer[BUFFER_SIZE];
-				snprintf(buffer, BUFFER_SIZE, "%d,%d,%d,%d,%d,%d,",
+				snprintf(buffer, BUFFER_SIZE, "%d,%d,%d,%d,%d,%d,%d,",
 					debug::freeRam(),
 					hardware::sim808::device.getSignalQuality().attenuation,
 					entry.metadata.batteryLevel,
 					entry.metadata.batteryVoltage,
 					static_cast<uint16_t>(entry.metadata.temperature * 100),
-					static_cast<uint8_t>(entry.metadata.status));
+					static_cast<uint8_t>(entry.metadata.status),
+					entry.metadata.timeToFix);
 
 				strcat(buffer, entry.position);
 
 				NOTICE_FORMAT("appendPosition", "Sending : %s", buffer);
 				uint16_t responseCode = hardware::sim808::device.httpPost(
 					config::main::value.network.url,
-					F("text/csv"),
+					F("text/gpstracker"),
 					buffer,
 					buffer,
 					BUFFER_SIZE
