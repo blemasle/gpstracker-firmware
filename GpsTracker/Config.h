d8945d75d2f5e5277e30c8bc8c61d6d53bde34b6
Bertrand Lemasle <blemasle@gmail.com>
2018-08-03 21:06:27 +1200

    Added code size comments on config structs

--- GpsTracker/Config.h
+++ GpsTracker/Config.h
@@ -59,18 +59,18 @@ struct sleepTimings_t {
 };
 
 struct config_t {
-	uint8_t seed;
-	char version[5];
-	uint16_t firstEntry;
-	uint16_t lastEntry;
+	uint8_t seed;					//sizeof = 1
+	char version[5];				//sizeof = 5
+	uint16_t firstEntry;			//sizeof = 2
+	uint16_t lastEntry;				//sizeof = 2
 #if BACKUP_ENABLE_NETWORK
-	networkConfig_t network;
+	networkConfig_t network;		//sizeof = 73
 #endif
-	uint8_t alertBatteryLevel1;
-	uint8_t alertBatteryLevel2;
-	uint8_t alertBatteryLevelClear;
-	char contactPhone[15];
-};
+	uint8_t alertBatteryLevel1;		//sizeof = 1
+	uint8_t alertBatteryLevel2;		//sizeof = 1
+	uint8_t alertBatteryLevelClear; //sizeof = 1
+	char contactPhone[15];			//sizeof = 15
+};									//sizeof = 28 + 73 = 101
 
 namespace config {
 
