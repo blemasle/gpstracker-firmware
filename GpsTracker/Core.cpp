d2f3afae4233103a8d9b7116f0dda84a24bf137f
Bertrand Lemasle <blemasle@gmail.com>
2018-08-05 19:09:54 +1200

    Moved line return at the beginning of each line.

--- GpsTracker/Core.cpp
+++ GpsTracker/Core.cpp
@@ -55,32 +55,33 @@ namespace core {
 	uint8_t notifyFailures(PositionEntryMetadata &metadata) {
 		SIM808RegistrationStatus networkStatus;
 		char buffer[SMS_BUFFER_SIZE];
-		const __FlashStringHelper * backupFailureString = F(" Backup battery failure ?\n");
+		const __FlashStringHelper * backupFailureString = F(" Backup battery failure ?");
 
 		uint8_t triggered = alerts::getTriggered(metadata);
 		if (!triggered) return NO_ALERTS_NOTIFIED;
+
+		NOTICE_FORMAT("notifyFailures", "triggered : %B", triggered);
 
 		network::powerOn();
 		networkStatus = network::waitForRegistered(NETWORK_DEFAULT_TOTAL_TIMEOUT_MS);
 
 		if (!network::isAvailable(networkStatus.stat)) return NO_ALERTS_NOTIFIED;
 
-		strncpy_P(buffer, PSTR("Alerts !\n"), SMS_BUFFER_SIZE);
+		strncpy_P(buffer, PSTR("Alerts !"), SMS_BUFFER_SIZE);
 		if (bitRead(triggered, ALERT_BATTERY_LEVEL_1) || bitRead(triggered, ALERT_BATTERY_LEVEL_2)) {
-			details::appendToSmsBuffer(buffer, PSTR("- Battery at %d%%.\n"), metadata.batteryLevel);
+			details::appendToSmsBuffer(buffer, PSTR("\n- Battery at %d%%."), metadata.batteryLevel);
 		}
 
 		if (bitRead(triggered, ALERT_RTC_TEMPERATURE_FAILURE)) {
-			details::appendToSmsBuffer(buffer, PSTR("- Temperature is %dC.%S"), static_cast<uint16_t>(metadata.temperature * 100), backupFailureString);
+			details::appendToSmsBuffer(buffer, PSTR("\n- Temperature is %dC.%S"), static_cast<uint16_t>(metadata.temperature * 100), backupFailureString);
 		}
 
 		if (bitRead(triggered, ALERT_RTC_CLOCK_FAILURE)) {
-			details::appendToSmsBuffer(buffer, PSTR("-RTC was stopped.%S"), backupFailureString);
+			details::appendToSmsBuffer(buffer, PSTR("\n- RTC was stopped.%S"), backupFailureString);
 		}
 
 		bool notified = network::sendSms(buffer);
 		if (!notified) NOTICE_MSG("notifyFailure", "SMS not sent !");
-		NOTICE_FORMAT("notifyFailures", "%s", buffer);
 
 		network::powerOff();
 		return notified ? triggered : NO_ALERTS_NOTIFIED; //If not notified, the alerts state should not be persisted (so we can retry to notify them)
