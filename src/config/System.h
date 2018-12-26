421d070fc190a2d33291ea9aea1c9cc28d3c088e
Bertrand Lemasle <blemasle@gmail.com>
2018-12-26 18:29:09 +1300

    Version bump to 1.42

--- src/config/System.h
+++ src/config/System.h
@@ -3,7 +3,7 @@
 #define CONFIG_ADDR 0				///< Address of the config block in the I2C EEPROM chip.
 #define CONFIG_RESERVED_SIZE 128	///< Reserved size for the config block in the I2C EEPROM chip.
 #define CONFIG_SEED 14				///< Seed use to detect invalid or outdate configuration data.
-#define VERSION "1.41"				/// Version string, only used for indicative purpose.
+#define VERSION "1.42"				/// Version string, only used for indicative purpose.
 
 
 /**
